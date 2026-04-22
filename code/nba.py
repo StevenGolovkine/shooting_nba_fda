042aab2fecf70a7f24f16c2a4132bd5538394273
StevenGolovkine <steven_golovkine@icloud.com>
2026-04-22 15:28:01 -0400

    Review

--- code/nba.py
+++ code/nba.py
@@ -228,11 +228,67 @@ class ShotCharts:
             cmap='seismic', norm=midnorm
         )
         ax.text(0.03, 0.925, f"{title}", fontsize='large', transform=ax.transAxes)
-        #ax.text(0.03, 0.875, f"{name}", fontsize='medium', transform=ax.transAxes)
         ax = ShotCharts.create_court(ax, 'black')
         if add_headshot:
             ax = ShotCharts.add_headshot(ax, idx)
         return ax
+
+    def shots_chart_2(
+        ax_shots: mpl.axes,
+        ax_shots_made: mpl.axes,
+        df_shots: pd.DataFrame,
+        df_shots_made: pd.DataFrame,
+        name: str,
+        title_shots: str,
+        title_shots_made: str,
+        add_headshot: bool = True
+    ) -> np.ndarray:
+        cond = df_shots.PLAYER_NAME == name
+
+        density_shots = df_shots[cond]['DENSITY'].iloc[0]
+        density_shots_made = df_shots_made[cond]['DENSITY'].iloc[0]
+        idx_shots = df_shots[cond]['PLAYER_ID'].iloc[0]
+        idx_shots_made = df_shots_made[cond]['PLAYER_ID'].iloc[0]
+
+        maximum = max(
+            np.max(np.abs(density_shots)),
+            np.max(np.abs(density_shots_made))
+        )
+
+        X_MIN, X_MAX = (-250, 250)
+        Y_MIN, Y_MAX = (0, 470)
+        XX, YY = np.mgrid[X_MIN:X_MAX:201j, Y_MIN:Y_MAX:201j]
+
+        midnorm = MidpointNormalize(vmin=-1., vcenter=0, vmax=1)
+        
+        # Shots missed
+        ax_shots.contourf(
+            XX, YY, density_shots / maximum,
+            levels=30,
+            cmap='seismic', norm=midnorm
+        )
+        ax_shots.text(
+            0.03, 0.925, f"{title_shots}",
+            fontsize='large', transform=ax_shots.transAxes
+        )
+        ax_shots = ShotCharts.create_court(ax_shots, 'black')
+        if add_headshot:
+            ax_shots = ShotCharts.add_headshot(ax_shots, idx_shots)
+
+        # Shots made
+        ax_shots_made.contourf(
+            XX, YY, density_shots_made / maximum,
+            levels=30,
+            cmap='seismic', norm=midnorm
+        )
+        ax_shots_made.text(
+            0.03, 0.925, f"{title_shots_made}",
+            fontsize='large', transform=ax_shots_made.transAxes
+        )
+        ax_shots_made = ShotCharts.create_court(ax_shots_made, 'black')
+        if add_headshot:
+            ax_shots_made = ShotCharts.add_headshot(ax_shots_made, idx_shots_made)
+        return ax_shots, ax_shots_made
 
     def shots_chart_fdata(
         ax: mpl.axes,
@@ -280,11 +336,72 @@ class ShotCharts:
             cmap='seismic', norm=midnorm
         )
         ax.text(0.03, 0.925, f"{title}", fontsize='large', transform=ax.transAxes)
-        #ax.text(0.03, 0.875, f"{name}", fontsize='medium', transform=ax.transAxes)
         ax = ShotCharts.create_court(ax, 'black')
         if add_headshot:
             ax = ShotCharts.add_headshot(ax, idx)
         return ax
+
+    def shots_chart_reconstruction_2(
+        ax_shots: mpl.axes,
+        ax_shots_made: mpl.axes,
+        df_shots: pd.DataFrame,
+        df_shots_made: pd.DataFrame,
+        df_reconstruction_shots: DenseFunctionalData,
+        df_reconstruction_shots_made: DenseFunctionalData,
+        name: str,
+        title_shots: str,
+        title_shots_made: str,
+        add_headshot: bool = True
+    ) -> tuple[mpl.axes, mpl.axes]:
+        cond_shots = df_shots.PLAYER_NAME == name
+        cond_shots_made = df_shots_made.PLAYER_NAME == name
+
+        idx_shots = df_shots[cond_shots]['PLAYER_ID'].iloc[0]
+        idx_shots_made = df_shots_made[cond_shots_made]['PLAYER_ID'].iloc[0]
+        indice_shots = df_shots.index[cond_shots]
+        indice_shots_made = df_shots_made.index[cond_shots_made]
+        density_shots = df_reconstruction_shots[indice_shots[0]].values.squeeze()
+        density_shots_made = (
+            df_reconstruction_shots_made[indice_shots_made[0]].values.squeeze()
+        )
+
+        maximum = max(
+            np.max(np.abs(density_shots)),
+            np.max(np.abs(density_shots_made))
+        )
+
+        X_MIN, X_MAX = (-250, 250)
+        Y_MIN, Y_MAX = (0, 470)
+        XX, YY = np.mgrid[X_MIN:X_MAX:201j, Y_MIN:Y_MAX:201j]
+
+        midnorm = MidpointNormalize(vmin=-1., vcenter=0, vmax=1)
+        ax_shots.contourf(
+            XX, YY, density_shots / maximum,
+            levels=30,
+            cmap='seismic', norm=midnorm
+        )
+        ax_shots.text(
+            0.03, 0.925, f"{title_shots}",
+            fontsize='large', transform=ax_shots.transAxes
+        )
+        ax_shots = ShotCharts.create_court(ax_shots, 'black')
+        if add_headshot:
+            ax_shots = ShotCharts.add_headshot(ax_shots, idx_shots)
+
+        ax_shots_made.contourf(
+            XX, YY, density_shots_made / maximum,
+            levels=30,
+            cmap='seismic', norm=midnorm
+        )
+        ax_shots_made.text(
+            0.03, 0.925, f"{title_shots_made}",
+            fontsize='large', transform=ax_shots_made.transAxes
+        )
+        ax_shots_made = ShotCharts.create_court(ax_shots_made, 'black')
+        if add_headshot:
+            ax_shots_made = ShotCharts.add_headshot(ax_shots_made, idx_shots_made)
+
+        return ax_shots, ax_shots_made
 
     def mean_chart(
         ax: mpl.axes,
@@ -311,12 +428,56 @@ class ShotCharts:
             0.03, 0.925, f"{title}", fontsize='large', transform=ax.transAxes
         )
         ax = ShotCharts.create_court(ax, 'black')
-        # ax.text(
-        #     0.03, 0.87,
-        #     f"$\mu^{{({{{idx_c + 1}}})}}$",
-        #     fontsize='medium', transform=ax.transAxes
-        # )
         return ax
+
+    def mean_chart_2(
+        ax_mean_1: mpl.axes,
+        ax_mean_2: mpl.axes,
+        mfpca: MFPCA,
+        title_1: str,
+        title_2: str
+    ) -> tuple[mpl.axes, mpl.axes]:
+        if isinstance(mfpca, MFPCA):
+            mean_1 = mfpca.mean.data[0][0].values.squeeze()
+            mean_2 = mfpca.mean.data[1][0].values.squeeze()
+        else:
+            mean_1 = mfpca.mean[0].values.squeeze()
+            mean_2 = mfpca.mean[1].values.squeeze()
+
+        maximum = max(
+            np.max(np.abs(mean_1)),
+            np.max(np.abs(mean_2))
+        )
+
+        X_MIN, X_MAX = (-250, 250)
+        Y_MIN, Y_MAX = (0, 470)
+        XX, YY = np.mgrid[X_MIN:X_MAX:201j, Y_MIN:Y_MAX:201j]
+
+        midnorm = MidpointNormalize(vmin=-1., vcenter=0, vmax=1)
+
+        ax_mean_1.contourf(
+            XX, YY, mean_1 / maximum,
+            levels=30,
+            cmap='seismic', norm=midnorm
+        )
+        ax_mean_1.text(
+            0.03, 0.925, f"{title_1}",
+            fontsize='large', transform=ax_mean_1.transAxes
+        )
+        ax_mean_1 = ShotCharts.create_court(ax_mean_1, 'black')
+
+        ax_mean_2.contourf(
+            XX, YY, mean_2 / maximum,
+            levels=30,
+            cmap='seismic', norm=midnorm
+        )
+        ax_mean_2.text(
+            0.03, 0.925, f"{title_2}",
+            fontsize='large', transform=ax_mean_2.transAxes
+        )
+        ax_mean_2 = ShotCharts.create_court(ax_mean_2, 'black')
+
+        return ax_mean_1, ax_mean_2
 
     def components_chart(
         ax: mpl.axes,
@@ -350,12 +511,67 @@ class ShotCharts:
             0.03, 0.925, f"{title}", fontsize='large', transform=ax.transAxes
         )
         ax = ShotCharts.create_court(ax, 'black')
-        # ax.text(
-        #     0.03, 0.87,
-        #     f"$\phi_{{{idx + 1}}}^{{({{{idx_c + 1}}})}} ({pct[idx]:.1f}\%)$",
-        #     fontsize='medium', transform=ax.transAxes
-        # )
         return ax
+
+    def components_chart_2(
+        ax_component_1: mpl.axes,
+        ax_component_2: mpl.axes,
+        mfpca: MFPCA,
+        idx: int,
+        title_1: str,
+        title_2: str
+    ) -> tuple[mpl.axes, mpl.axes]:
+        if isinstance(mfpca, MFPCA):
+            if isinstance(mfpca.eigenfunctions.data[0], BasisFunctionalData):
+                eigenfunctions_1 = mfpca.eigenfunctions.data[0].to_grid()
+            else:
+                eigenfunctions_1 = mfpca.eigenfunctions.data[0]
+
+            if isinstance(mfpca.eigenfunctions.data[1], BasisFunctionalData):
+                eigenfunctions_2 = mfpca.eigenfunctions.data[1].to_grid()
+            else:
+                eigenfunctions_2 = mfpca.eigenfunctions.data[1]
+        else:
+            eigenfunctions_1 = mfpca.eigenfunctions[0]
+            eigenfunctions_2 = mfpca.eigenfunctions[1]
+
+        eigenfunctions_1 = eigenfunctions_1[idx].values.squeeze()
+        eigenfunctions_2 = eigenfunctions_2[idx].values.squeeze()
+
+        maximum = max(
+            np.max(np.abs(eigenfunctions_1)),
+            np.max(np.abs(eigenfunctions_2))
+        )
+
+        X_MIN, X_MAX = (-250, 250)
+        Y_MIN, Y_MAX = (0, 470)
+        XX, YY = np.mgrid[X_MIN:X_MAX:201j, Y_MIN:Y_MAX:201j]
+
+        midnorm = MidpointNormalize(vmin=-1., vcenter=0, vmax=1)
+
+        ax_component_1.contourf(
+            XX, YY, eigenfunctions_1 / maximum,
+            levels=30,
+            cmap='seismic', norm=midnorm
+        )
+        ax_component_1.text(
+            0.03, 0.925, f"{title_1}",
+            fontsize='large', transform=ax_component_1.transAxes
+        )
+        ax_component_1 = ShotCharts.create_court(ax_component_1, 'black')
+
+        ax_component_2.contourf(
+            XX, YY, eigenfunctions_2 / maximum,
+            levels=30,
+            cmap='seismic', norm=midnorm
+        )
+        ax_component_2.text(
+            0.03, 0.925, f"{title_2}",
+            fontsize='large', transform=ax_component_2.transAxes
+        )
+        ax_component_2 = ShotCharts.create_court(ax_component_2, 'black')
+
+        return ax_component_1, ax_component_2
 
     def shots_decomposition_chart(
         ax: mpl.axes,
@@ -391,11 +607,6 @@ class ShotCharts:
         )
         ax = ShotCharts.create_court(ax, 'black')
         ax.text(0.03, 0.925, f"{title}", fontsize='large', transform=ax.transAxes)
-        # ax.text(
-        #     0.03, 0.87,
-        #     f"$\mathfrak{{c}}_{{{idx + 1}}}\phi_{{{idx + 1}}}^{{({{{idx_c + 1}}})}} ({pct[idx]:.1f}\%)$",
-        #     fontsize='medium', transform=ax.transAxes
-        # )
         return ax
 
     def shots_decomposition_cluster(
